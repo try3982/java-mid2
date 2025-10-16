a9d7d51871d65a100a8ce60a34e448337a46ef2f
WonJin <try3982@kakao.com>
2025-10-16 15:34:38 +0900

    - 다형성을 통한 중복해결 시도 1    -. 현재 반환타입이 잘 안맞는 문제    -. 잘못도니 타입의 인수 전달 문제
     - 코드 재사용을 늘리기 위해  Object와 다형성을 사용하면 타입 안정성이 떨어지는 문제가 발생

---
+++ src/generic/ObjectBox.java
@@ -0,0 +1,14 @@
+package generic;
+
+public class ObjectBox {
+
+    private Object value;
+
+    public Object get() {
+        return value;
+    }
+
+    public void set(Object value) {
+        this.value = value;
+    }
+}
